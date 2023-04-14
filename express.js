c238d2775999fa4184c961d75bb04c8ebf9871eb
Quas <aaron.hickman201@gmail.com>
2023-04-14 18:28:32 +0100

    setup

--- express.js
+++ express.js
@@ -5,6 +5,13 @@ const uuid = require("uuid");
 
 
 
+const app = express();
+
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded data with querystring library
+app.use(express.json()); // Middleware to parse incoming JSON data
+app.use(express.static("public")); // Middleware to serve static files from the "public" directory
+
+
 
 
 
